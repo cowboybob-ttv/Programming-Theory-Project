0879a36b8c43262e7fbcb25413b0be3a24802d66
cowboybob-ttv <105476211+cowboybob-ttv@users.noreply.github.com>
2022-07-06 18:03:41 -0500

    final project submission

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public GameObject finishedPopUp;
+    private float xPopUp = 20f;
     private Animator playerAnim;
     private SpriteRenderer playerSprite;
     private float xBound = -13f;
@@ -23,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {        
         PlayerMovement();
         ConstrainPlayer();
+        GameFinished();
     }
 
     public void PlayerMovement()
@@ -50,5 +53,12 @@ public class PlayerController : MonoBehaviour
         } 
     }
 
-    
+    public void GameFinished()
+    {
+        if (transform.position.x > xPopUp)
+        {
+            finishedPopUp.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }    
 }
